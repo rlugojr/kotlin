f4842bd8f277a65e7966433f0431dff1ca208d45
Dmitry Jemerov <yole@jetbrains.com>
2015-11-23 14:10:58 +0100

    don't show "kotlin not configured" notification for modules where the runtime is outdated

--- idea/src/org/jetbrains/kotlin/idea/actions/ConfigureKotlinInProjectAction.kt
+++ idea/src/org/jetbrains/kotlin/idea/actions/ConfigureKotlinInProjectAction.kt
@@ -41,7 +41,7 @@ public abstract class ConfigureKotlinInProjectAction : AnAction() {
         val configurators = getApplicableConfigurators(project)
 
         when {
-            configurators.size == 1 -> configurators.first().configure(project)
+            configurators.size == 1 -> configurators.first().configure(project, emptyList())
             configurators.isEmpty() -> Messages.showErrorDialog("There aren't configurators available", e.getPresentation().getText()!!)
             else -> {
                 Messages.showErrorDialog("More than one configurator is available", e.getPresentation().getText()!!)
