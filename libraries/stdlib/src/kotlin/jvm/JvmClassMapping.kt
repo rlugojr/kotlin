50bdab9b0898986af5badc20b01b576713de6ee4
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-11-26 16:11:55 +0300

    Move reflection utils to kotlin.jvm

--- libraries/stdlib/src/kotlin/jvm/JvmClassMapping.kt
+++ libraries/stdlib/src/kotlin/jvm/JvmClassMapping.kt
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 @file:kotlin.jvm.JvmName("ClassMapping")
+@file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
 
 package kotlin.jvm
 
@@ -25,12 +26,43 @@ import kotlin.reflect.KClass
 /**
  * Returns a Java [Class] instance corresponding to the given [KClass] instance.
  */
+@Suppress("UNCHECKED_CAST")
 @Intrinsic("kotlin.KClass.java.property")
 public val <T : Any> KClass<T>.java: Class<T>
+    @JvmName("getJavaClass")
     get() = (this as ClassBasedDeclarationContainer).jClass as Class<T>
 
 /**
  * Returns a [KClass] instance corresponding to the given Java [Class] instance.
  */
+@Suppress("UNCHECKED_CAST")
 public val <T : Any> Class<T>.kotlin: KClass<T>
+    @JvmName("getKotlinClass")
     get() = Reflection.createKotlinClass(this) as KClass<T>
+
+
+@Deprecated("Provided for binary compatibility", level = DeprecationLevel.HIDDEN)
+public fun <T : Any> Class<T>.getKotlin(): KClass<T> = Reflection.createKotlinClass(this) as KClass<T>
+
+
+/**
+ * Returns the runtime Java class of this object.
+ */
+@Suppress("UNCHECKED_CAST")
+@Intrinsic("kotlin.javaClass.property")
+public val <T: Any> T.javaClass : Class<T>
+    get() = (this as java.lang.Object).getClass() as Class<T>
+
+/**
+ * Checks if array can contain element of type [T].
+ */
+@Intrinsic("kotlin.jvm.isArrayOf")
+public fun <reified T : Any> Array<*>.isArrayOf(): Boolean =
+        T::class.java.isAssignableFrom(this.javaClass.componentType)
+
+/**
+ * Returns the annotation type of this annotation.
+ */
+@Suppress("UNCHECKED_CAST")
+public fun <T : Annotation> T.annotationClass() : KClass<out T> =
+        (this as java.lang.annotation.Annotation).annotationType().kotlin as KClass<out T>
