4621e7cb27e7bc9283634347470a881d61d0daa5
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-11-27 18:02:41 +0300

    Extension contains for ranges: rename parameter item to value.

--- libraries/tools/kotlin-stdlib-gen/src/templates/Ranges.kt
+++ libraries/tools/kotlin-stdlib-gen/src/templates/Ranges.kt
@@ -154,7 +154,7 @@ fun ranges(): List<GenericFunction> {
 
     templates addAll integralPermutations.map { until(it.first, it.second) }
 
-    fun containsDeprecated(rangeType: PrimitiveType, itemType: PrimitiveType) = f("contains(item: $itemType)") {
+    fun containsDeprecated(rangeType: PrimitiveType, itemType: PrimitiveType) = f("contains(value: $itemType)") {
         operator(true)
 
         deprecate("Range<T> is deprecated. Use ClosedRange<T> instead.")
@@ -164,11 +164,11 @@ fun ranges(): List<GenericFunction> {
         onlyPrimitives(Ranges, rangeType)
         platformName("${rangeType.name.decapitalize()}RangeContains")
         returns("Boolean")
-        doc { "Checks if the specified [item] belongs to this range." }
-        body { "return start <= item && item <= end" }
+        doc { "Checks if the specified [value] belongs to this range." }
+        body { "return start <= value && value <= end" }
     }
 
-    fun contains(rangeType: PrimitiveType, itemType: PrimitiveType) = f("contains(item: $itemType)") {
+    fun contains(rangeType: PrimitiveType, itemType: PrimitiveType) = f("contains(value: $itemType)") {
         operator(true)
 
         check(rangeType.isNumeric() == itemType.isNumeric()) { "Required rangeType and itemType both to be numeric or both not, got: $rangeType, $itemType" }
@@ -177,8 +177,8 @@ fun ranges(): List<GenericFunction> {
         customReceiver("ClosedRange<T>")
         platformName("${rangeType.name.decapitalize()}RangeContains")
         returns("Boolean")
-        doc { "Checks if the specified [item] belongs to this range." }
-        body { "return start <= item && item <= endInclusive" }
+        doc { "Checks if the specified [value] belongs to this range." }
+        body { "return start <= value && value <= endInclusive" }
     }
 
 
